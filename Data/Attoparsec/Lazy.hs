d247c43ae06fc140d15a5322e2ea0d55fb193c0f
Bryan O'Sullivan <bos@serpentine.com>
2010-03-09 20:41:06 -0800

    Improve doco a bit.

--- Data/Attoparsec/Lazy.hs
+++ Data/Attoparsec/Lazy.hs
@@ -17,7 +17,9 @@
 -- lazily read file, e.g. via 'L.readFile' or 'L.hGetContents'.
 --
 -- Behind the scenes, strict 'B.ByteString' values are still used
--- internally to store parser input and handle it efficiently.
+-- internally to store parser input and manipulate it efficiently.
+-- High-performance parsers such as 'string' still expect strict
+-- 'B.ByteString' parameters.
 
 module Data.Attoparsec.Lazy
     (

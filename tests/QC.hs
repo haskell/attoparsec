951dc4822e7fc7e9bd59e4d1aab9123618d663c1
Bryan O'Sullivan <bos@serpentine.com>
2010-03-09 19:10:17 -0800

    Tidy up tests a bit.

--- tests/QC.hs
+++ tests/QC.hs
@@ -36,54 +36,55 @@ maybeP p s = case P.parse p s `P.feed` B.empty of
 
 defP p s = P.parse p s `P.feed` B.empty
 
-satisfy (w,s) = maybeP (P.satisfy (<=w)) (B.cons w s) == Just w
+satisfy w s = maybeP (P.satisfy (<=w)) (B.cons w s) == Just w
 
-word8 (w,s) = maybeP (P.word8 w) (B.cons w s) == Just w
+word8 w s = maybeP (P.word8 w) (B.cons w s) == Just w
 
 anyWord8 s = maybeP P.anyWord8 s == if B.null s
                                     then Nothing
                                     else Just (B.head s)
 
-notWord8 (w, NonEmpty s) = maybeP (P.notWord8 w) s == if v == w
+notWord8 w (NonEmpty s) = maybeP (P.notWord8 w) s == if v == w
                                                       then Nothing
                                                       else Just v
     where v = B.head s
 
 string s = maybeP (P.string s) s == Just s
 
-skipWhile (w,s) =
+skipWhile w s =
     let t = B.dropWhile (<=w) s
     in case defP (P.skipWhile (<=w)) s of
          P.Done t' () -> t == t'
          _            -> False
 
-takeCount (Positive k,s) =
+takeCount (Positive k) s =
     case maybeP (P.take k) s of
       Nothing -> k > B.length s
       Just s' -> k <= B.length s
 
-takeWhile (w,s) =
+takeWhile w s =
     let (h,t) = B.span (==w) s
     in case defP (P.takeWhile (==w)) s of
          P.Done t' h' -> t == t' && h == h'
          _            -> False
 
-takeWhile1 (w, s) =
+takeWhile1 w s =
     let s'    = B.cons w s
         (h,t) = B.span (<=w) s'
     in case defP (P.takeWhile1 (<=w)) s' of
          P.Done t' h' -> t == t' && h == h'
          _            -> False
 
-takeTill (w, s) =
+takeTill w s =
     let (h,t) = B.break (==w) s
     in case defP (P.takeTill (==w)) s of
          P.Done t' h' -> t == t' && h == h'
          _            -> False
 
-ensure n s = case defP (P.ensure n) s of
-               P.Done _ () -> B.length s >= n
-               _           -> B.length s < n
+ensure n s = case defP (P.ensure m) s of
+               P.Done _ () -> B.length s >= m
+               _           -> B.length s < m
+    where m = (n `mod` 220) - 20
 
 takeWhile1_empty = maybeP (P.takeWhile1 undefined) B.empty == Nothing
 
@@ -111,4 +112,5 @@ tests = [
     testProperty "endOfInput" endOfInput,
     testProperty "ensure" ensure
     ]
+
   ]

12979efd53c3f359aeb0e25e51760ce82f5311e3
Bryan O'Sullivan <bos@serpentine.com>
2014-05-27 00:13:00 -0700

    pappend for ByteString

--- Data/Attoparsec/ByteString/Buffer.hs
+++ Data/Attoparsec/ByteString/Buffer.hs
@@ -26,6 +26,7 @@ module Data.Attoparsec.ByteString.Buffer
     , buffer
     , unbuffer
     , length
+    , pappend
     , unsafeIndex
     , substring
     , unsafeDrop
@@ -72,29 +73,35 @@ instance Monoid Buffer where
     mempty = unsafePerformIO $ Buf nullForeignPtr 0 0 0 0 <$> newIORef 0
     {-# NOINLINE mempty #-}
 
-    mappend (Buf _ _ _ 0 _ _) b = b
-    mappend a (Buf _ _ _ 0 _ _) = a
-    mappend (Buf fp0 off0 len0 cap0 gen ref) (Buf fp1 off1 len1 _ _ _) =
-      inlinePerformIO . withForeignPtr fp0 $ \ptr0 ->
-        withForeignPtr fp1 $ \ptr1 -> do
-          gen' <- atomicModifyIORef ref (\a -> let a' = a+1 in (a',a'))
-          let newlen = len0 + len1
-          if gen' == gen+1 && newlen <= cap0
-            then do
-              memcpy (ptr0 `plusPtr` (off0+len0))
-                     (ptr1 `plusPtr` off1)
-                     len1
-              return (Buf fp0 off0 newlen cap0 gen' ref)
-            else do
-              let newcap = newlen * 2
-              fp <- mallocPlainForeignPtrBytes newcap
-              withForeignPtr fp $ \ptr -> do
-                memcpy ptr (ptr0 `plusPtr` off0) len0
-                memcpy (ptr `plusPtr` len0) (ptr1 `plusPtr` off1) len1
-              return (Buf fp 0 newlen newcap gen' ref)
+    mappend     (Buf _ _ _ 0 _ _) b    = b
+    mappend a   (Buf _ _ _ 0 _ _)      = a
+    mappend buf (Buf fp off len _ _ _) = append buf fp off len
 
     mconcat [] = mempty
     mconcat xs = foldl1' mappend xs
+
+pappend :: Buffer -> ByteString -> Buffer
+pappend buf (PS fp off len) = append buf fp off len
+
+append :: Buffer -> ForeignPtr a -> Int -> Int -> Buffer
+append (Buf fp0 off0 len0 cap0 gen ref) fp1 off1 len1 =
+  inlinePerformIO . withForeignPtr fp0 $ \ptr0 ->
+    withForeignPtr fp1 $ \ptr1 -> do
+      gen' <- atomicModifyIORef ref (\a -> let a' = a+1 in (a',a'))
+      let newlen = len0 + len1
+      if gen' == gen+1 && newlen <= cap0
+        then do
+          memcpy (ptr0 `plusPtr` (off0+len0))
+                 (ptr1 `plusPtr` off1)
+                 len1
+          return (Buf fp0 off0 newlen cap0 gen' ref)
+        else do
+          let newcap = newlen * 2
+          fp <- mallocPlainForeignPtrBytes newcap
+          withForeignPtr fp $ \ptr -> do
+            memcpy ptr (ptr0 `plusPtr` off0) len0
+            memcpy (ptr `plusPtr` len0) (ptr1 `plusPtr` off1) len1
+          return (Buf fp 0 newlen newcap gen' ref)
 
 length :: Buffer -> Int
 length (Buf _ _ len _ _ _) = len

1c15a580b7d11f956e4acf66a32effc4ace6a13c
Sebastiaan Joosten <sjj@ou.nl>
2014-06-03 11:53:59 +0200

    Added some comments

--- Data/Attoparsec/Internal/Types.hs
+++ Data/Attoparsec/Internal/Types.hs
@@ -144,11 +144,17 @@ plus f g = Parser $ \t pos more lose succ ->
   let lose' t' _pos' more' _ctx _msg = runParser g t' pos more' lose succ
   in runParser f t pos more lose' succ
 
+-- | the idea for <||> is to have it act more like parsec’s <|>
+-- It should fail when the left parser fails after consuming input
+-- Should the left parser fail without consuming input, it will perform the right parser.
+-- I used the notation <||> because it was suggested in this thread:
+-- https://groups.google.com/forum/#!topic/haskell-cafe/Q7MmNIYGXMg
 infixl 3 <||>
 (<||>) :: Parser i a -> Parser i a -> Parser i a
 (<||>) f g = Parser $ \t pos more lose succ ->
   let lose' t' pos' more' ctx msg
-        = if fromPos pos' == fromPos pos then runParser g t' pos' more' lose succ
+        = if fromPos pos' == fromPos pos
+          then runParser g t' pos' more' lose succ
           else lose t' pos' more' ctx msg
   in runParser f t pos more lose' succ
 {-# INLINE (<||>) #-}

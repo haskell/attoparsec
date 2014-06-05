4909395d50abef2ec78ac212b493215348b9e774
Sebastiaan Joosten <sjj@ou.nl>
2014-06-05 13:34:32 +0200

    Added some tests

--- tests/QC/Combinator.hs
+++ tests/QC/Combinator.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module QC.Combinator where
 
 import Control.Applicative
@@ -31,9 +32,45 @@ match n (NonNegative x) (NonNegative y) rs =
             B8.replicate x 'x', B8.pack (show n), B8.replicate y 'y'
           ]
 
+-- make sure <||> does not backtrack:
+nonBacktrack :: Positive (Small Int) -> Positive (Small Int) -> Repack -> Bool
+nonBacktrack (Positive (Small a)) (Positive (Small b)) rs =
+    parseBS (P.match parser) (repackBS rs input) == Nothing
+  where parser = (P.skipWhile (=='x') <* P.signed P.decimal) C.<||>
+                 P.skipWhile (const True) -- never failing parser on rhs
+        input = B.concat [
+            B8.replicate a 'x', B8.replicate b 'y'
+          ]
+-- make sure <||> succeeds if the first parser succeeds:
+pickFirst :: Positive (Small Int) -> NonNegative Int -> Repack -> Bool
+pickFirst (Positive (Small a)) (NonNegative b) rs =
+    parseBS (P.match parser) (repackBS rs input) == Just (input,())
+  where parser = (P.skipWhile (=='x') C.<||>
+                 (const () <$> P.satisfy (const False))) -- always failing parser on rhs
+                 <* P.skipWhile (=='y')
+        input = B.concat [
+            B8.replicate a 'x', B8.replicate b 'y'
+          ]
+-- make sure <||> takes the second parser if the first fails without consuming input:
+pickLast :: NonNegative Int -> NonNegative Int -> Repack -> Bool
+pickLast (NonNegative a) (NonNegative b) rs =
+    parseBS (P.match parser) (repackBS rs input) == Just (input,True)
+  where parser = ( (const False <$> P.string "xyx" -- the entire string is matched at once!
+                   )
+                  C.<||>
+                   (const True <$> P.skipWhile (=='x'))
+                 )
+                 <* P.skipWhile (=='y')
+        input = B.concat [
+            B8.replicate a 'x', B8.replicate b 'y'
+          ]
+
 tests :: [Test]
 tests = [
     testProperty "choice" choice
   , testProperty "count" count
   , testProperty "match" match
+  , testProperty "pickLast" pickLast
+  , testProperty "pickFirst" pickLast
+  , testProperty "nonBacktrack" nonBacktrack
   ]
--- tests/TestFastSet.hs
+++ tests/TestFastSet.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import Data.Word (Word8)
-import qualified Data.Attoparsec.FastSet as F
+import qualified Data.Attoparsec.ByteString.FastSet as F
 import System.Random (Random(..), RandomGen)
 import Test.QuickCheck
 
@@ -9,13 +9,6 @@ integralRandomR :: (Integral a, RandomGen g) => (a, a) -> g -> (a, g)
 integralRandomR (a,b) g = case randomR (fromIntegral a :: Int,
                                         fromIntegral b :: Int) g
                           of (x,g') -> (fromIntegral x, g')
-
-instance Random Word8 where
-  randomR = integralRandomR
-  random = randomR (minBound,maxBound)
-
-instance Arbitrary Word8 where
-    arbitrary = choose (minBound, maxBound)
 
 prop_AllMembers s =
     let set = F.fromList s

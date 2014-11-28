bd88c48bd3f8e43efd75f6f2d11224c1747aab7c
SeanRBurton <burton.seanr@gmail.com>
2014-11-28 06:57:43 +0000

    Update TestFastSet.hs

--- tests/TestFastSet.hs
+++ tests/TestFastSet.hs
@@ -1,25 +1,28 @@
 module Main (main) where
 
 import Data.Word (Word8)
-import qualified Data.Attoparsec.FastSet as F
-import System.Random (Random(..), RandomGen)
+import qualified Data.Attoparsec.ByteString.FastSet as F
+import qualified Data.Attoparsec.Text.FastSet as F
 import Test.QuickCheck
 
-integralRandomR :: (Integral a, RandomGen g) => (a, a) -> g -> (a, g)
-integralRandomR (a,b) g = case randomR (fromIntegral a :: Int,
-                                        fromIntegral b :: Int) g
-                          of (x,g') -> (fromIntegral x, g')
-
-instance Random Word8 where
-  randomR = integralRandomR
-  random = randomR (minBound,maxBound)
-
-instance Arbitrary Word8 where
-    arbitrary = choose (minBound, maxBound)
-
-prop_AllMembers s =
+prop_members s =
     let set = F.fromList s
     in all (`F.memberWord8` set) s
-
+    
+prop_nonmembers s s' =
+    let set = F.fromList s
+    in not . any (`F.memberWord8` set) $ filter (not . (`elem` s)) s'
+    
+prop_membersText s =
+    let set = F.fromList s
+    in all (`FT.member` set) s
+    
+prop_nonmembersText s s' =
+    let set = FT.fromList s
+    in not . any (`FT.member` set) $ filter (not . (`elem` s)) s'
+    
 main = do
-  quickCheck prop_AllMembers
+  quickCheck prop_members
+  quickCheck prop_nonmembers
+  quickCheck prop_membersText
+  quickCheck prop_nonmembersText

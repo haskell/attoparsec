85bc992fc7dc158d89966a07bdbbe677e93d677d
Bryan O'Sullivan <bos@serpentine.com>
2015-03-27 14:09:07 -0700

    Add Text genome search benchmarks

--- benchmarks/Genome.hs
+++ benchmarks/Genome.hs
@@ -10,27 +10,50 @@ import Criterion.Main
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Char8 as L8
-import Data.Attoparsec.ByteString.Char8
-import qualified Data.Attoparsec.ByteString.Lazy as L
-import Common (rechunkBS)
+import Data.Attoparsec.ByteString.Char8 as B
+import qualified Data.Attoparsec.ByteString.Lazy as BL
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import Data.Attoparsec.Text as T
+import qualified Data.Attoparsec.Text.Lazy as TL
+import Common (rechunkBS, rechunkT)
 
 genome :: Benchmark
 genome = bgroup "genome" [
-    bench "B" $ nf (map (parse searchBS)) (B8.tails geneB)
-  , bench "BL" $ nf (map (L.parse searchBS)) (L8.tails geneBL)
-  , bgroup "CI" [
-      bench "B" $ nf (map (parse searchBSCI)) (B8.tails geneB)
-    , bench "BL" $ nf (map (L.parse searchBSCI)) (L8.tails geneBL)
-  ]
+    bgroup "bytestring" [
+        bench "s" $ nf (map (B.parse searchBS)) (B8.tails geneB)
+      , bench "l" $ nf (map (BL.parse searchBS)) (L8.tails geneBL)
+      , bgroup "CI" [
+          bench "s" $ nf (map (B.parse searchBSCI)) (B8.tails geneB)
+        , bench "l" $ nf (map (BL.parse searchBSCI)) (L8.tails geneBL)
+      ]
+    ]
+  , bgroup "text" [
+        bench "s" $ nf (map (T.parse searchT)) (T.tails geneT)
+      , bench "l" $ nf (map (TL.parse searchT)) (TL.tails geneTL)
+      , bgroup "CI" [
+          bench "s" $ nf (map (T.parse searchTCI)) (T.tails geneT)
+        , bench "l" $ nf (map (TL.parse searchTCI)) (TL.tails geneTL)
+      ]
+    ]
   ]
   where geneB  = B8.pack gene
         geneBL = rechunkBS 4 geneB
+        geneT  = T.pack gene
+        geneTL = rechunkT 4 geneT
 
-searchBS :: Parser ByteString
+searchBS :: B.Parser ByteString
 searchBS = "caac" *> ("aaca" <|> "aact")
 
-searchBSCI :: Parser ByteString
-searchBSCI = stringCI "CAAC" *> (stringCI "AACA" <|> stringCI "AACT")
+searchBSCI :: B.Parser ByteString
+searchBSCI = B.stringCI "CAAC" *> (B.stringCI "AACA" <|> B.stringCI "AACT")
+
+searchT :: T.Parser Text
+searchT = "caac" *> ("aaca" <|> "aact")
+
+searchTCI :: T.Parser Text
+searchTCI = T.asciiCI "CAAC" *> (T.asciiCI "AACA" <|> T.asciiCI "AACT")
 
 -- Dictyostelium discoideum developmental protein DG1094 (gacT) gene,
 -- partial cds. http://www.ncbi.nlm.nih.gov/nuccore/AF081586.1

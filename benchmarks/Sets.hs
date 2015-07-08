86056baa5d9767a73d5cc657684f0d3959602013
Bryan O'Sullivan <bos@serpentine.com>
2015-07-08 16:53:38 -0700

    Prepare to drop the Text FastSet implementation

--- benchmarks/Sets.hs
+++ benchmarks/Sets.hs
@@ -3,6 +3,7 @@ module Sets (benchmarks) where
 import Criterion
 import Data.Char (ord)
 import qualified Data.Attoparsec.Text.FastSet as FastSet
+import qualified TextFastSet
 import qualified Data.HashSet as HashSet
 import qualified Data.IntSet as IntSet
 
@@ -12,7 +13,9 @@ smallSet = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9']
 benchmarks :: Benchmark
 benchmarks = bgroup "sets" [
     bench "Fast" $ whnf (FastSet.member '*') (FastSet.fromList smallSet)
-  , bench "HashSet" $ whnf (HashSet.member '*') (HashSet.fromList smallSet)
-  , bench "IntSet" $ whnf (IntSet.member (ord '*'))
-                     (IntSet.fromList (map ord smallSet))
+  , bench "Hash" $ whnf (HashSet.member '*') (HashSet.fromList smallSet)
+  , bench "Int" $ whnf (IntSet.member (ord '*'))
+                  (IntSet.fromList (map ord smallSet))
+  , bench "TextFast" $ whnf (TextFastSet.member '*')
+                       (TextFastSet.fromList smallSet)
   ]

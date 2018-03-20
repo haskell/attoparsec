6e2dc20db0cb57973fd092d35cfc4c8798bd37b2
Andrew Martin <andrew.thaddeus@gmail.com>
2018-03-19 20:48:43 -0400

    Change character predicate to not use inClass. Add note about the impossibility of matching the performance of warp.

--- benchmarks/HeadersByteString.hs
+++ benchmarks/HeadersByteString.hs
@@ -9,6 +9,15 @@ import qualified Data.Attoparsec.ByteString.Char8 as B
 import qualified Data.Attoparsec.ByteString.Lazy as BL
 import qualified Data.ByteString.Char8 as B
 
+-- Note: In the benchmarks for parsing an http request
+-- from a strict bytestring, we consider warp's implementation,
+-- which has highly optimized code for handling the first
+-- line in particular. It's treatment of the headers
+-- is more relaxed from the treatment they are given by the
+-- attoparsec parser benchmarked here. Consequently, it
+-- is should not be possible to match its performance since
+-- it accepts header names with disallowed characters.
+
 headers :: IO Benchmark
 headers = do
   req <- B.readFile =<< pathTo "http-request.txt"
